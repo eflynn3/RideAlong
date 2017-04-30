acd50093d38cd42e7aee4da7eb9280ace1aef18e
Erin Turley <erinturley@live.com>
2017-04-29 23:21:57 -0400

    new view

---
+++ RideAlong/RequestRideViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RequestRideViewController.swift
+//  RideAlong
+//
+//  Created by Erin Turley on 4/29/17.
+//
+//
+
+import UIKit
+import Firebase
+
+class RequestRideViewController: UIViewController {
+
+}
