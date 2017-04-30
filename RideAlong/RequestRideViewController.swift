0f309731d99cca5eb1f49592c196be99048581e7
Erin Turley <erinturley@live.com>
2017-04-30 00:16:35 -0400

    request page

--- RideAlong/RequestRideViewController.swift
+++ RideAlong/RequestRideViewController.swift
@@ -10,5 +10,21 @@ import UIKit
 import Firebase
 
 class RequestRideViewController: UIViewController {
-
+    var userRef = FIRDatabase.database().reference()
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var passengersField: UITextField!
+   
+    @IBAction func handlePublish(sender: AnyObject) {
+        let location = self.locationField.text!
+        let passengers = self.passengersField.text!
+    }
+    
+    override func viewDidAppear( animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        let ref = self.userRef.child("requests") // 2
+        let Item = []
+        ref.setValue(Item) // 4
+    }
 }
